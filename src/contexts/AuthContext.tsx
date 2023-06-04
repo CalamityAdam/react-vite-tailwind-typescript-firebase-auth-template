ce36ab00e7a11ddade5c99dea4813cb6279b7abb
Adam Sisk <adamsisk91@gmail.com>
2023-06-03 21:41:28 -0500

    updates firebase config to match contract

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,83 +1,73 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { auth } from '../firebase'
+import React, { useContext, useState, useEffect } from 'react';
+import { auth } from '../firebase';
 import {
+  getRedirectResult,
+  GithubAuthProvider,
   GoogleAuthProvider,
   signInWithPopup,
-  GithubAuthProvider,
-} from 'firebase/auth'
+  signInWithRedirect,
+} from 'firebase/auth';
 
 interface IAuthProviderProps {
-  children: JSX.Element
+  children: JSX.Element;
 }
 
-const AuthContext = React.createContext({})
+const AuthContext = React.createContext({});
 
 export function useAuth(): any {
-  return useContext(AuthContext)
+  return useContext(AuthContext);
 }
 
 export function AuthProvider({ children }: IAuthProviderProps): JSX.Element {
-  const [currentUser, setCurrentUser] = useState<any>()
-  const [loading, setLoading] = useState(true)
+  const [currentUser, setCurrentUser] = useState<any>();
+  const [loading, setLoading] = useState(true);
 
-  function signup(email: string, password: string): Promise<any> {
-    return auth.createUserWithEmailAndPassword(email, password)
-  }
-
-  function googleSignin(): Promise<any> {
-    const provider = new GoogleAuthProvider()
-    return signInWithPopup(auth, provider)
+  async function googleSignin(): Promise<any> {
+    const provider = new GoogleAuthProvider();
+    try {
+      await signInWithPopup(auth, provider);
+    } catch (error: any) {
+      if (error.code === 'auth/popup-blocked') {
+        await signInWithRedirect(auth, provider);
+      } else {
+        console.error('error signing in with google: ', error);
+      }
+    }
   }
 
   function githubSignin(): Promise<any> {
-    const provider = new GithubAuthProvider()
-    return signInWithPopup(auth, provider)
-  }
-
-  function login(email: string, password: string): Promise<any> {
-    return auth.signInWithEmailAndPassword(email, password)
-  }
-
-  function logout(): Promise<any> {
-    return auth.signOut()
-  }
-
-  function resetPassword(email: string): Promise<any> {
-    return auth.sendPasswordResetEmail(email)
-  }
-
-  function updateEmail(email: string): Promise<any> {
-    return currentUser.updateEmail(email)
-  }
-
-  function updatePassword(password: string): Promise<any> {
-    return currentUser.updatePassword(password)
+    const provider = new GithubAuthProvider();
+    return signInWithPopup(auth, provider);
   }
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
-      setCurrentUser(user)
-      setLoading(false)
-    })
+      setCurrentUser(user);
+      setLoading(false);
+    });
 
-    return unsubscribe
-  }, [])
+    getRedirectResult(auth)
+      .then((result) => {
+        if (result?.user) {
+          setCurrentUser(result.user);
+        }
+      })
+      .catch((error) => {
+        console.error('error handling redirect result: ', error);
+      });
+
+    return unsubscribe;
+  }, []);
 
   const value = {
     currentUser,
-    login,
-    signup,
     googleSignin,
     githubSignin,
-    logout,
-    resetPassword,
-    updateEmail,
-    updatePassword,
-  }
+  };
 
   return (
     <AuthContext.Provider value={value}>
       {!loading && children}
     </AuthContext.Provider>
-  )
+  );
 }
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,7 +1,11 @@
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/auth'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const app = firebase.initializeApp({
+const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
@@ -9,7 +13,12 @@ const app = firebase.initializeApp({
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
-})
+};
 
-export const auth = app.auth()
-export default app
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export { analytics, auth };
+export default app;
